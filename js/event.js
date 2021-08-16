973341c1ddc1637ce71f53610a2c495e8b1f18af
Nishikantaray <nishikantaray1@gmail.com>
2021-08-16 18:16:35 +0530

    added shuffle-the-array

--- js/event.js
+++ js/event.js
@@ -1,47 +1,113 @@
-var month = ["Jan","Feb","March","April","May","June","July","Aug","Sep","Oct","Nov","December"];
-// console.log(month);
-var htmlc=`<select class="custom-select mr-sm-2 ">`;        
+let month = ["Jan","Feb","March","April","May","June","July","Aug","Sep","Oct","Nov","December",];
+let htmlc = `<select class="custom-select mr-sm-2 " id="type">`;
+htmlc += `<option>Select</option>`;
 for (let index = 0; index < month.length; index++) {
-    // console.log(month[index]);
-    htmlc +=`<option value="${index}">${month[index]}</option>`;
+  htmlc += `<option value="${index}">${month[index]}</option>`;
 }
-htmlc+="</select>";
+
+htmlc += "</select>";
 $("#selectoption").append(htmlc);
-let data=[{},{}];
-try {
-    // console.log(data);
-    var html = '<div class="row">';
-    for (i = 0; i < data.length; i++) {
-      if (i % 3 == 0 && i != 0) {
-        html += "</div>";
-        html += '<div class="row">';
-      }
-     
-      html += `<div class="col-lg-9 col-sm-12 m-auto">
-      
-              <div class="over-item txt-right unique-style3">
-                    <div class="row">
-                        <div class="col-md-8 col-12">
-                        <div class="break"></div>
-                        <h3 class="text-left" >Events Name</h3>
-                        <p class="text-left">Find all the Open Source Events happening around the globe month-wise and never miss
-                            any opportunity to learn and grow from the experts 😉</p>
-                        <div class="over-btn text-left">
-                            <a href="#">Read More</a>
-                        </div>
-                        </div>
-                        <div class="col-md-4 col-12">
-                        <img src="../images/t1.jpg" height="200px" alt="" srcset=""></div>
-                    </div>
-                  
-                  
-              </div></div>`;
+
+let data = [
+  {
+    monthId: "0",
+    Data: [
+      {
+        event: "1",
+        details: {
+          eventName: "Events Name1",
+          eventDate: "date-12/03/2021",
+          eventDesc: "Find all the Open Source Events happening around the globe month-wise and never miss any opportunity to learn and grow from the experts 😉",
+          eventurl: "#",
+          eventImg: "../images/t1.jpg",
+        },
+      },
+      {
+        event: "2",
+        details: {
+          eventName: "Events Name2",
+          eventDate: "date-12/03/2021",
+          eventDesc: "Find all the Open Source Events happening around the globe month-wise and never miss any opportunity to learn and grow from the experts 😉",
+          eventurl: "#",
+          eventImg: "../images/t1.jpg",
+        },
+      },
+    ],
+  },
+  {
+    monthId: "1",
+    Data: [
+      {
+        event: "1",
+        details: {
+          eventName: "Events Name",
+          eventDate: "",
+          eventDesc: "",
+          eventurl: "",
+          eventImg: "",
+        },
+      },
+    ],
+  },
+];
+
+let valu;
+$(function () {
+    $("#type").change(function () {
+      valu=$(this).val();
+      console.log(valu);
+      for (let index = 0; index < 12; index++) {
+        if(data[index].monthId==valu){
+            console.log("true");
+            console.log(data[0].Data.length);
+           for (let i = 0; i < data[index].Data.length; i++) {
+            try {
+                // console.log(data);
+                var html = '<div class="row">';
+                for (i = 0; i < data[index].Data.length; i++) {
+                    
+                  if (i % 3 == 0 && i != 0) {
+                    html += "</div>";
+                    html += '<div class="row">';
+                  }
+              
+                  html += `<div class="col-lg-9 col-12 m-auto">
+                    
+                            <div class="over-item txt-right unique-style3">
+                                  <div class="row">
+                                      <div class="col-md-8 col-12">
+                                      <div class="break"></div>
+                                      <h3 class="text-left" >${data[index].Data[i].details.eventName}</h3>
+                                      <h6 class="text-left text-light" >${data[index].Data[i].details.eventDate}</h6></br>
+                                      <p class="text-left">${data[index].Data[i].details.eventDesc}</p>
+                                      <div class="over-btn text-left">
+                                          <a href="${data[index].Data[i].details.eventurl}">Read More</a>
+                                      </div>
+                                      </div>
+                                      <div class="col-md-4 col-12">
+                                      <img src="${data[index].Data[i].details.eventImg}" height="200px" alt="" srcset=""></div>
+                                  </div>
+                                
+                                
+                            </div></div>`;
+                }
+              
+                $("#rowdy").append(html);
+              } catch (error) {
+                console.log(error);
+              }
+            }
+            
+              
+        }else{
+            var htmlz=`<div class= "container">` ;
+            htmlz += `<p class="text-light">No data found please select another month</p>`;
+            htmlz +=`</div>`;
+            $("#error").append(htmlz);
+        }
     }
-
-    $("#rowdy").append(html);
-  } catch (error) {
-    console.log(error);
-  }
+    });
+    
+});
 
 
-getData();
