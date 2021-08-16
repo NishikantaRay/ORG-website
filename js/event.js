f7fd3ecff8445c6127c0b829eb3b0e678302e6b5
Sunil200228 <sunilpanda20022002@gmail.com>
2021-08-16 23:37:30 +0530

    fixed tutorial

--- js/event.js
+++ js/event.js
@@ -55,12 +55,12 @@ let valu;
 $(function () {
     $("#type").change(function () {
       valu=$(this).val();
-      console.log(valu);
-      for (let index = 0; index < 12; index++) {
-        if(data[index].monthId==valu){
-            console.log("true");
-            console.log(data[0].Data.length);
-           for (let i = 0; i < data[index].Data.length; i++) {
+      // console.log(valu);
+      let index = valu;
+      $("#rowdy").empty();
+      $("#error").empty();
+      if(valu < data.length && data[index].Data.length > 0){
+        for (let i = 0; i < data[index].Data.length; i++) {
             try {
                 // console.log(data);
                 var html = '<div class="row">';
@@ -97,15 +97,66 @@ $(function () {
                 console.log(error);
               }
             }
-            
-              
-        }else{
+      }else{
             var htmlz=`<div class= "container">` ;
             htmlz += `<p class="text-light">No data found please select another month</p>`;
             htmlz +=`</div>`;
             $("#error").append(htmlz);
         }
-    }
+    //   for (let index = 0; index < data.length; index++) {
+    //     console.log("monthid" +data[index].monthId);
+    //     console.log("index"+index);
+    //     if(data[index].monthId==valu){
+    //         console.log("true");
+    //         console.log(data[0].Data.length);
+    //        for (let i = 0; i < data[index].Data.length; i++) {
+    //         try {
+    //             // console.log(data);
+    //             var html = '<div class="row">';
+    //             for (i = 0; i < data[index].Data.length; i++) {
+                    
+    //               if (i % 3 == 0 && i != 0) {
+    //                 html += "</div>";
+    //                 html += '<div class="row">';
+    //               }
+              
+    //               html += `<div class="col-lg-9 col-12 m-auto">
+                    
+    //                         <div class="over-item txt-right unique-style3">
+    //                               <div class="row">
+    //                                   <div class="col-md-8 col-12">
+    //                                   <div class="break"></div>
+    //                                   <h3 class="text-left" >${data[index].Data[i].details.eventName}</h3>
+    //                                   <h6 class="text-left text-light" >${data[index].Data[i].details.eventDate}</h6></br>
+    //                                   <p class="text-left">${data[index].Data[i].details.eventDesc}</p>
+    //                                   <div class="over-btn text-left">
+    //                                       <a href="${data[index].Data[i].details.eventurl}">Read More</a>
+    //                                   </div>
+    //                                   </div>
+    //                                   <div class="col-md-4 col-12">
+    //                                   <img src="${data[index].Data[i].details.eventImg}" height="200px" alt="" srcset=""></div>
+    //                               </div>
+                                
+                                
+    //                         </div></div>`;
+    //             }
+              
+    //             $("#rowdy").append(html);
+    //           } catch (error) {
+    //             console.log(error);
+    //           }
+    //         }
+            
+              
+    //     }else{
+    //       console.log("index"+index);
+    //       console.log("month"+data[index].monthId);
+    //         var htmlz=`<div class= "container">` ;
+    //         htmlz += `<p class="text-light">No data found please select another month</p>`;
+    //         htmlz +=`</div>`;
+    //         $("#error").append(htmlz);
+    //     }
+    // }
     });
     
 });
