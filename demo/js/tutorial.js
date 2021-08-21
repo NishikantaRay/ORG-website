584992c1ad52374b0a7d8acb57562a831e2a2fee
Nishikantaray <nishikantaray1@gmail.com>
2021-08-22 00:41:15 +0530

    added by nishikanta

--- demo/js/tutorial.js
+++ demo/js/tutorial.js
@@ -71,7 +71,7 @@ const getData = async () => {
       //                    </div>   `;
       html += `<div class="mt-5 card text-center">
       <a href=https://www.youtube.com/embed/${data[i].id}?autoplay=1>
-      <img id="ytimg" height="215" width="100%" src=https://img.youtube.com/vi/${data[i].id}/hqdefault.jpg alt='ytvideo'>
+      <img id="ytimg" height="100%" width="100%" src=https://img.youtube.com/vi/${data[i].id}/hqdefault.jpg alt='ytvideo'>
       <div class" text-center" >
       <span id="butt">▶</span>
       </div>
